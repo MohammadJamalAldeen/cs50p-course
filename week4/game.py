043ab0c026f735105fa4cadcaff4bd7102b4fff2
Mohammad Jamal Aldeen <jamalaldenmohamed@gmail.com>
2026-09-22 21:12:43 +0300

    Create game.py

---
+++ week4/game.py
@@ -0,0 +1,26 @@
+from random import randint
+rand = randint(1,100)
+while True:
+    try:
+        level = int(input("Level: "))
+        if level < 1:
+            continue
+        else:
+            break
+    except ValueError:
+        continue
+rand = randint(1,level)
+while True:
+    try:
+        n = int(input("Guess: "))
+        if n <= 0:
+            continue
+        if n == rand:
+            print("Just right!")
+            break
+        elif n > rand:
+            print("Too large!")
+        else:
+            print("Too small!")
+    except ValueError:
+        continue
